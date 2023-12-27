7db971fc191ca2b8cad80aa6095886934e4f16c8
sure <104919283+SURE3187774683@users.noreply.github.com>
2023-12-27 18:05:48 +0800

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,7 @@
-# 第五版：减去一个MODEL类之后的修正版,加入tensorboard
+#接口参数
+#Double-DQN实现pathplanning
+#SURE   
+
 import torch
 import os
 import time
@@ -16,7 +19,7 @@ DISCOUNT = 0.95                             #折扣因子
 LEARNING_RATE = 1e-3                        #学习率(步长)
 UPDATE_TARGET_MODE_EVERY = 20               #model更新频率
 STATISTICS_EVERY = 20                       #记录在tensorboard的频率
-model_save_avg_reward = 100                 #优秀模型评价指标
+MODEL_SAVE_AVG_REWARD = 150                 #优秀模型评价指标
 EPI_START = 1                               #epsilon的初始值
 EPI_END = 0.001                             #epsilon的终止值
 EPI_DECAY = 0.995                           #epsilon的缩减速率
@@ -28,10 +31,8 @@ SHOW_EVERY = 100                            #显示频率
 
 env = envCube()
 agent = DQNAgent(env.OBSERVATION_SPACE_VALUES, env.ACTION_SPACE_VALUES, REPLAY_MEMORY_SIZE, BATCH_SIZE, DISCOUNT,LEARNING_RATE , EPI_START, EPI_END, EPI_DECAY)
-
-
-writer = SummaryWriter('logs/')
-for episode in range(EPISODE_N):
+writer = SummaryWriter('logs/')                         #创建笔
+for episode in range(EPISODE_N): 
     state = env.reset()                                 #重置环境
     done = False
     episode_reward = 0                                  #每局奖励清零
@@ -40,12 +41,16 @@ for episode in range(EPISODE_N):
         action = agent.select_action(state)              #选择action
         next_state, reward, done = env.step(action)     #游戏走一步
         agent.push_transition(state, action, reward, next_state, done)   #将当前状态放入池       
-        agent.update_model()                             #更新model
+        agent.update_epsilon()                           #更新epsilon
         state = next_state                              #更新state
         episode_reward += reward                        #累加当次训练的reward
+
+        if done:
+            state = env.reset()                                 #重置环境
+            agent.episode_rewards.append(episode_reward)         #收集所有训练累计的rewar
+            break
     
-    agent.update_epsilon()                           #更新epsilon
-    agent.episode_rewards.append(episode_reward)         #收集所有训练累计的rewar
+    agent.update_model()                             #更新model
     if episode % UPDATE_TARGET_MODE_EVERY == 0:         #更新target_model(将当前模型的复制到目标模型)
         agent.update_target_model()
     if episode%SHOW_EVERY==0:                           #打印日志
@@ -56,6 +61,7 @@ for episode in range(EPISODE_N):
             print(f"### Episode Reward: {agent.episode_rewards[-1]}")
         if VISUALIZE:                                   #显示动画
             env.render()
+
     if episode % STATISTICS_EVERY == 0:                 #记录有用的参数
         avg_reward = sum(agent.episode_rewards[-STATISTICS_EVERY:])/len(agent.episode_rewards[-STATISTICS_EVERY:])
         max_reward = max(agent.episode_rewards[-STATISTICS_EVERY:])
@@ -67,8 +73,8 @@ for episode in range(EPISODE_N):
         writer.add_scalar('Epsilon', agent.epsilon, episode)
         writer.add_scalar('Loss', agent.loss_value, episode)
         
-        if avg_reward > model_save_avg_reward:          #保存优秀的模型
-            model_save_avg_reward = avg_reward
+        if avg_reward > MODEL_SAVE_AVG_REWARD:          #保存优秀的模型
+            MODEL_SAVE_AVG_REWARD = avg_reward
             model_dir = './models'
             if not os.path.exists(model_dir):
                 os.makedirs(model_dir)
