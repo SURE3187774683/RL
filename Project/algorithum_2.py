51c59f4a5839fdd448986c85a473f82cd33e0a21
sure <104919283+SURE3187774683@users.noreply.github.com>
2023-12-28 17:40:42 +0800

    Update algorithum_2.py

--- Project/algorithum_2.py
+++ Project/algorithum_2.py
@@ -1,16 +1,24 @@
-#DDQN算法
+import numpy as np
+import time
+import os
+import pickle
+from matplotlib import style
+style.use('ggplot')
 
 import torch
-import os
-import time
 from tensorboardX import SummaryWriter
 import numpy as np
-from agent import DQNAgent
-from agent import DQN
 from env import envCube
 
 class Algorithum_2:
-    def __init__(self,episodes, replay_memory_size, batch_size,discount, learning_rate,update_target_mode_every,statistics_every,model_save_avg_reward,epi_start, epi_end, epi_decay,visualize,verbose,show_every):
+    def __init__(self, episodes, replay_memory_size, batch_size, discount, learning_rate, update_target_mode_every,
+                 statistics_every, model_save_avg_reward, epi_start, epi_end, epi_decay, visualize, verbose, show_every):
+
+        path = os.path.realpath(__file__)
+        filename = os.path.splitext(os.path.basename(path))[0]
+        self.writer = SummaryWriter(f'logs/{filename}')
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
         self.episodes = episodes
         self.replay_memory_size = replay_memory_size
         self.batch_size = batch_size
@@ -25,58 +33,63 @@ class Algorithum_2:
         self.visualize = visualize
         self.verbose = verbose
         self.show_every = show_every
-
-        self.writer = SummaryWriter('logs/experiment1/')                         #创建笔
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
+    
     def train(self):
         env = envCube()
-        agent = DQNAgent(env.OBSERVATION_SPACE_VALUES, env.ACTION_SPACE_VALUES, self.replay_memory_size, self.batch_size, self.discount,self.learning_rate , self.epi_start, self.epi_end, self.epi_decay,self.device)
-        for episode in range(self.episodes): 
-            state = env.reset()                                 #重置环境
-            done = False
-            episode_reward = 0                                  #每局奖励清零
+        q_table = env.get_qtable()
+        q_table = {k: torch.tensor(v).to(self.device) for k, v in q_table.items()}
+        episode_rewards = []
+        epsilon = self.epi_start
 
+        for episode in range(self.episodes):
+            obs = env.reset()
+            done = False
+            episode_reward = 0
             while not done:
-                action = agent.select_action(state)             #选择action
-                next_state, reward, done = env.step(action)     #游戏走一步
-                agent.push_transition(state, action, reward, next_state, done)   #将当前状态放入池       
-                agent.update_epsilon()                          #更新epsilon
-                agent.update_model()                            #更新model
-                state = next_state                              #更新state
-                episode_reward += reward                        #累加当次训练的reward
+                if np.random.random() > epsilon:
+                    action = np.argmax(q_table[obs].cpu().numpy())
+                else:
+                    action = np.random.randint(0, env.ACTION_SPACE_VALUES)
+
+                new_obs, reward, done = env.step(action)
+
+                # Update the Q_table
+                current_q = q_table[obs][action]
+                max_future_q = torch.max(q_table[new_obs])
+
+                if reward == env.FOOD_REWARD:
+                    new_q = env.FOOD_REWARD
+                else:
+                    new_q = current_q + self.learning_rate * (reward + self.discount * max_future_q - current_q)
+
+                q_table[obs][action] = new_q.to(self.device)  # 将更新后的值移动到GPU上
+                obs = new_obs
+                epsilon = max(self.epi_end, epsilon * self.epi_decay)
+                episode_reward += reward
 
                 if done:
-                    agent.episode_rewards.append(episode_reward)#收集所有训练累计的rewar
+                    episode_rewards.append(episode_reward)
                     break
-
-            if episode % self.update_target_mode_every == 0:         #更新target_model(将当前模型的复制到目标模型)
-                agent.update_target_model()
-            if episode%self.show_every==0:                           #打印日志
-                print(f"Episode: {episode}        Epsilon:{agent.epsilon}")
-                if self.verbose == 1:                                #输出平均奖励
-                    print(f"### Average Reward: {np.mean(agent.episode_rewards)}")                
-                if self.verbose == 2:                                #输出每轮游戏的奖励
-                    print(f"### Episode Reward: {agent.episode_rewards[-1]}")
-                if self.visualize:                                   #显示动画
+                
+            if episode % self.show_every == 0:
+                print(f"Episode: {episode}        Epsilon:{epsilon}")
+                if self.verbose == 1:
+                    avg_reward = np.mean(episode_rewards)
+                    print(f"### Average Reward: {avg_reward}")
+                if self.verbose == 2:
+                    print(f"### Episode Reward: {episode_rewards[-1]}")
+                if self.visualize:
                     env.render()
 
-            if episode % self.statistics_every == 0:                 #记录有用的参数
-                avg_reward = sum(agent.episode_rewards[-self.statistics_every:])/len(agent.episode_rewards       [-self.statistics_every:])
-                max_reward = max(agent.episode_rewards[-self.statistics_every:])
-                min_reward = min(agent.episode_rewards[-self.statistics_every:])
+            if episode % self.statistics_every == 0:
+                avg_reward = sum(episode_rewards[-self.statistics_every:]) / self.statistics_every
+                max_reward = max(episode_rewards[-self.statistics_every:])
+                min_reward = min(episode_rewards[-self.statistics_every:])
                 self.writer.add_scalar('Episode Reward', episode_reward, episode)
                 self.writer.add_scalar('Average Reward', avg_reward, episode)
                 self.writer.add_scalar('Max Reward', max_reward, episode)
                 self.writer.add_scalar('Min Reward', min_reward, episode)
-                self.writer.add_scalar('Epsilon', agent.epsilon, episode)
-                self.writer.add_scalar('Loss', agent.loss_value, episode)
+                self.writer.add_scalar('Epsilon', epsilon, episode)
 
-                if avg_reward > self.model_save_avg_reward:          #保存优秀的模型
-                    self.model_save_avg_reward = avg_reward
-                    model_dir = './models'
-                    if not os.path.exists(model_dir):
-                        os.makedirs(model_dir)
-                    model_path = os.path.join(model_dir, f'{avg_reward:7.3f}_{int(time.time())}.model')
-                    torch.save(DQN(env.OBSERVATION_SPACE_VALUES,env.ACTION_SPACE_VALUES).state_dict(), model_path)
+#with open(f'qtable_{int(time.time())}.pickle','wb') as f:
+#    pickle.dump(q_table,f)
