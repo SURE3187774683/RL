783ed6e6ca9ea9e7a890299dc7f868b86f35d501
sure <104919283+SURE3187774683@users.noreply.github.com>
2023-12-30 19:33:20 +0800

    Update algorithum_2.py

--- Project/algorithum_2.py
+++ Project/algorithum_2.py
@@ -1,133 +1,99 @@
-#A*算法
+#Q-learning算法
 
-from PIL import Image
 import numpy as np
-from queue import PriorityQueue
-import cv2
+import time
+import os
+import pickle
+from matplotlib import style
+style.use('ggplot')
+
+import torch
+from tensorboardX import SummaryWriter
+import numpy as np
 from env import envCube
 
-class AStarNode:
-    def __init__(self, position, g_score=float('inf'), f_score=float('inf'), parent=None):
-        self.position = position
-        self.g_score = g_score  # 从起始节点到当前节点的实际代价
-        self.f_score = f_score  # 从起始节点到当前节点的估计代价（实际代价 + 启发式代价）
-        self.parent = parent
+class Algorithum_2:
+    def __init__(self, episodes, discount, learning_rate,statistics_every, model_save_avg_reward, epi_start, epi_end, epi_decay, visualize, verbose, show_every):
 
-    def __lt__(self, other):
-        return self.f_score < other.f_score
+        path = os.path.realpath(__file__)
+        filename = os.path.splitext(os.path.basename(path))[0]
+        self.writer = SummaryWriter(f'logs/{filename}')
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def heuristic_cost_estimate(start, goal):  # 从agent到food的启发式距离
-    return abs(start[0] - goal[0]) + abs(start[1] - goal[1])
-
-def action_to_delta(action):
-    if action == 0:
-        return (0, 1)
-    elif action == 1:
-        return (0, -1)
-    elif action == 2:
-        return (1, 0)
-    elif action == 3:
-        return (-1, 0)
-    elif action == 4:
-        return (1, 1)
-    elif action == 5:
-        return (-1, 1)
-    elif action == 6:
-        return (1, -1)
-    elif action == 7:
-        return (-1, -1)
-    elif action == 8:
-        return (0, 0)
-
-def reconstruct_path(current):
-    path = []
-    while current.parent:
-        path.append(current.position)
-        current = current.parent
-    path.append(current.position)
-    return path[::-1]
-
-def is_valid_position(position, size):
-    x, y = position
-    return 0 <= x < size and 0 <= y < size
-
-import cv2
-import numpy as np
-from PIL import Image
-
-def render(path, agent, food, enemies, size):
-    img = Image.new('RGB', (size, size), (0, 0, 0))  # 创建一个空白的RGB图像
-
-    for enemy in enemies:                   # 绘制敌人-红色
-        img.putpixel((enemy[1], enemy[0]),  (255, 0, 0))
-
-    img.putpixel((agent[1], agent[0]),(0, 0, 255))   # 绘制智能体-蓝色
-    img.putpixel((food[1], food[0]), (0, 255, 0))   # 绘制食物-绿色
-
-    img_array = np.array(img)  # 将PIL图像转换为NumPy数组
-
-    for i in range(len(path) - 1):                   # 绘制路径-白色
-        pt1 = (path[i][1], path[i][0])
-        pt2 = (path[i+1][1], path[i+1][0])
-        cv2.line(img_array, pt1, pt2, (255, 255, 255), 1, cv2.LINE_AA)
-
-    img = Image.fromarray(img_array)  # 将NumPy数组转换为PIL图像
-
-    img = img.resize((800, 800))
-    img.show()
-    cv2.waitKey(0)
-    img.save("trajectory_3.png")  # 保存带有轨迹的图像
-
-
-def astar_search(agent, food, enemies, size, env):
-    open_set = PriorityQueue()
-    start_node = AStarNode(agent, 0, heuristic_cost_estimate(agent, food))
-    open_set.put(start_node)
-
-    closed_set = set()
-    g_scores = {agent: 0}
-
-    while not open_set.empty():
-        current_node = open_set.get()
-        current_position = current_node.position
-
-        if current_position == food:
-            return reconstruct_path(current_node)
-
-        closed_set.add(current_position)
-
-        for action in range(9):
-            next_position = tuple(np.add(current_position, action_to_delta(action)))
-
-            if not is_valid_position(next_position, size) or next_position in enemies:
-                continue
-
-            g_score = g_scores[current_position] + 1
-            if g_score < g_scores.get(next_position, float('inf')):
-                g_scores[next_position] = g_score
-                f_score = g_score + heuristic_cost_estimate(next_position, food)
-                neighbor = AStarNode(next_position, g_score, f_score, current_node)
-                open_set.put(neighbor)
-
-class Algorithum_3:
+        self.episodes = episodes
+        self.discount = discount
+        self.learning_rate = learning_rate
+        self.statistics_every = statistics_every
+        self.model_save_avg_reward = model_save_avg_reward
+        self.epi_start = epi_start
+        self.epi_end = epi_end
+        self.epi_decay = epi_decay
+        self.visualize = visualize
+        self.verbose = verbose
+        self.show_every = show_every
+    
     def train(self):
         env = envCube()
-        env.reset()
-        agent = (env.players[0].get_x(), env.players[0].get_y())
-        food = (env.food.get_x(), env.food.get_y())
-        enemies = set()
-        for enemy in env.enemies:
-            enemies.add((enemy.get_x(), enemy.get_y()))
-        path = astar_search(agent, food, enemies, env.SIZE, env)
-        print("Path:", path)
-        render(path, agent, food, enemies, env.SIZE)
+        q_table = env.get_qtable()
+        q_table = {k: torch.tensor(v).to(self.device) for k, v in q_table.items()}
+        episode_rewards = []
+        epsilon = self.epi_start
 
+        for episode in range(self.episodes):
+            obs = env.reset()
+            done = False
+            episode_reward = 0
+            while not done:
+                if np.random.random() > epsilon:
+                    action = np.argmax(q_table[obs].cpu().numpy())
+                else:
+                    action = np.random.randint(0, env.ACTION_SPACE_VALUES)
 
+                new_obs, reward, done = env.step(action)
 
+                # Update the Q_table
+                current_q = q_table[obs][action]
+                max_future_q = torch.max(q_table[new_obs])
 
+                if reward == env.FOOD_REWARD:
+                    new_q = torch.tensor(env.FOOD_REWARD, dtype=torch.float32).to(self.device)
+                else:
+                    new_q = current_q + self.learning_rate * (reward + self.discount * max_future_q - current_q)
+                    new_q = new_q.to(self.device)
+                epsilon = max(self.epi_end, epsilon * self.epi_decay)
 
+                q_table[obs][action] = new_q.to(self.device)  # 将更新后的值移动到GPU上
+                obs = new_obs
+                episode_reward += reward
+                
+                if done:
+                    episode_rewards.append(episode_reward)
+                    break
 
+            if episode % self.show_every == 0:
+                print(f"Episode: {episode}        Epsilon:{epsilon}")
+                if self.verbose == 1:
+                    avg_reward = np.mean(episode_rewards)
+                    print(f"### Average Reward: {avg_reward}")
+                if self.verbose == 2:
+                    print(f"### Episode Reward: {episode_rewards[-1]}")
+                if self.visualize:
+                    env.render()
 
+            if episode % self.statistics_every == 0:
+                avg_reward = sum(episode_rewards[-self.statistics_every:]) / self.statistics_every
+                max_reward = max(episode_rewards[-self.statistics_every:])
+                min_reward = min(episode_rewards[-self.statistics_every:])
+                self.writer.add_scalar('Episode Reward', episode_reward, episode)
+                self.writer.add_scalar('Average Reward', avg_reward, episode)
+                self.writer.add_scalar('Max Reward', max_reward, episode)
+                self.writer.add_scalar('Min Reward', min_reward, episode)
+                self.writer.add_scalar('Epsilon', epsilon, episode)
 
+                if avg_reward > self.model_save_avg_reward and avg_reward < 90:          #保存优秀的模型
 
-    
+                    env.render_trajectory(2)  # 保存智能体轨迹图像
+                    self.model_save_avg_reward = avg_reward
+
+#with open(f'qtable_{int(time.time())}.pickle','wb') as f:
+#    pickle.dump(q_table,f)
