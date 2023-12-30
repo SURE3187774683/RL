40d6c01a2ccad13f68e94c56a4e2ad9c439102a0
sure <104919283+SURE3187774683@users.noreply.github.com>
2023-12-30 17:24:45 +0800

    Update algorithum_3.py

--- Project/algorithum_3.py
+++ Project/algorithum_3.py
@@ -1,114 +1,98 @@
-import heapq
-from env import envCube
-import cv2
-import numpy as np
-from PIL import Image
+#A*算法
 
-# 定义节点类
-class Node:
-    def __init__(self, position, g_cost, h_cost, parent=None):
+
+import numpy as np
+from queue import PriorityQueue
+from env import envCube
+
+class AStarNode:
+    def __init__(self, position, g_score=float('inf'), f_score=float('inf'), parent=None):
         self.position = position
-        self.g_cost = g_cost
-        self.h_cost = h_cost
-        self.f_cost = g_cost + h_cost
+        self.g_score = g_score  #从起始节点到当前节点的实际代价
+        self.f_score = f_score  #从起始节点到当前节点的估计代价（实际代价 + 启发式代价）
         self.parent = parent
 
     def __lt__(self, other):
-        return self.f_cost < other.f_cost
+        return self.f_score < other.f_score
 
-# 定义A*算法函数
-def astar_search(env, start, goal):
-    open_list = []
-    closed_list = set()
+def heuristic_cost_estimate(start, goal):#从agent到food的启发式距离
+    return abs(start[0] - goal[0]) + abs(start[1] - goal[1])
 
-    # 创建起始节点
-    start_node = Node(start, 0, manhattan_distance(start, goal))
-    heapq.heappush(open_list, start_node)
+def reconstruct_path(current):
+    path = []
+    while current.parent:
+        path.append(current.position)
+        current = current.parent
+    path.append(current.position)
+    return path[::-1]
 
-    while open_list:
-        current_node = heapq.heappop(open_list)
+def action_to_delta(action):
+    if action == 0:
+        return (0, 1)
+    elif action == 1:
+        return (0, -1)
+    elif action == 2:
+        return (1, 0)
+    elif action == 3:
+        return (-1, 0)
+    elif action == 4:
+        return (1, 1)
+    elif action == 5:
+        return (-1, 1)
+    elif action == 6:
+        return (1, -1)
+    elif action == 7:
+        return (-1, -1)
+    elif action == 8:
+        return (0, 0)
+
+def is_valid_position(position, size):
+    x, y = position
+    return 0 <= x < size and 0 <= y < size
+
+def astar_search(agent, food, enemies, size):
+    open_set = PriorityQueue()
+    start_node = AStarNode(agent, 0, heuristic_cost_estimate(agent, food))
+    open_set.put(start_node)
+
+    closed_set = set()
+    g_scores = {agent: 0}
+
+    while not open_set.empty():
+        current_node = open_set.get()
         current_position = current_node.position
 
-        if current_position == goal:
-            path = []
-            while current_node:
-                path.append(current_node.position)
-                current_node = current_node.parent
-            return path[::-1]
+        if current_position == food:
+            return reconstruct_path(current_node)
 
-        closed_list.add(current_position)
+        closed_set.add(current_position)
 
-        # 获取当前节点的相邻节点
-        neighbors = get_neighbors(env, current_position)
+        for action in range(9):
+            next_position = tuple(np.add(current_position, action_to_delta(action)))
 
-        for neighbor in neighbors:
-            if neighbor in closed_list:
+            if not is_valid_position(next_position, size) or next_position in enemies:
                 continue
 
-            g_cost = current_node.g_cost + 1
-            h_cost = manhattan_distance(neighbor, goal)
-            f_cost = g_cost + h_cost
-
-            neighbor_node = Node(neighbor, g_cost, h_cost, current_node)
-
-            if is_node_in_list(neighbor_node, open_list):
-                continue
-
-            heapq.heappush(open_list, neighbor_node)
+            g_score = g_scores[current_position] + 1
+            if g_score < g_scores.get(next_position, float('inf')):
+                g_scores[next_position] = g_score
+                f_score = g_score + heuristic_cost_estimate(next_position, food)
+                neighbor = AStarNode(next_position, g_score, f_score, current_node)
+                open_set.put(neighbor)
 
     return None
 
-# 获取节点的相邻节点
-def get_neighbors(env, position):
-    size = env.SIZE
-    x, y = position
-    neighbors = []
-
-    if x > 0:
-        neighbors.append((x - 1, y))
-    if x < size - 1:
-        neighbors.append((x + 1, y))
-    if y > 0:
-        neighbors.append((x, y - 1))
-    if y < size - 1:
-        neighbors.append((x, y + 1))
-
-    return neighbors
-
-# 计算两个位置之间的曼哈顿距离
-def manhattan_distance(position1, position2):
-    x1, y1 = position1
-    x2, y2 = position2
-    return abs(x1 - x2) + abs(y1 - y2)
-
-# 检查节点是否在列表中
-def is_node_in_list(node, node_list):
-    for n in node_list:
-        if n.position == node.position:
-            return True
-    return False
-
-def render(self):                   #显示图片
-        img = self.get_image()
-        img = img.resize((800, 800))
-        cv2.imshow('Predator', np.array(img))
-        cv2.waitKey(1)
-
-def get_image(self):
-        env = np.zeros((self.SIZE, self.SIZE, 3), dtype=np.uint8)
-        env[self.food.get_x()][self.food.get_y()] = self.d[self.FOOD_N]
-
-        for i in range(self.NUM_PLAYERS):        
-            env[self.players[i].get_x()][self.players[i].get_y()] = self.d[self.PLAYER_N]
-
-        for i in range(self.NUM_ENEMIES):
-            env[self.enemies[i].get_x()][self.enemies[i].get_y()] = self.d[self.ENEMY_N]
-
-        img = Image.fromarray(env, 'RGB')
-        return img
-
 env = envCube()
-start = (0, 0)
-goal = (9, 9)
-path = astar_search(env, start, goal)
-print("Path:", path)
+state = env.reset()
+agent = (env.players[0].get_x(), env.players[0].get_y())
+food = (env.food.get_x(), env.food.get_y())
+enemies = set()
+for enemy in env.enemies:
+    enemies.add((enemy.get_x(), enemy.get_y()))
+path = astar_search(agent, food, enemies, env.SIZE) # 使用A*算法进行路径规划
+
+if path is not None:    # 输出路径
+    print("Path:", path)
+else:
+    print("No path found.")
+
