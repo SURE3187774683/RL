e444da84625cf1609bfad2c4f37affd2380eb3ba
sure <104919283+SURE3187774683@users.noreply.github.com>
2024-01-18 22:05:09 +0800

    Update env.py

--- MADQN/env.py
+++ MADQN/env.py
@@ -3,11 +3,8 @@
 # 静态和动态环境可选择
 # 奖惩机制可自定义
 # SURE
-import pickle
-import cv2
-from PIL import Image
+
 import numpy as np
-from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 
 ENV_MOVE = False                            #env是否变化
@@ -15,7 +12,7 @@ MAX_STEP = 350                              #每局最大步数
 
 # 建立Cube类，用于创建player、food和enemy
 class Cube:
-    def __init__(self, size, x=None, y=None):  # 随机生成个体的位置
+    def __init__(self, size, x=None, y=None):  # 生成个体的位置
         self.size = size
         if x is not None and y is not None:
             self.x = x
@@ -23,10 +20,6 @@ class Cube:
         else:
             self.x = np.random.randint(0, self.size)
             self.y = np.random.randint(0, self.size)
-
-    def create_enemy_at_position(self, x, y):
-        enemy = Cube(self.size, x, y)
-        return enemy
 
     def get_x(self):
         return self.x
@@ -91,53 +84,18 @@ class envCube:  # 生成环境类
     CLOSER_REWARD = 5
     FARER_PENALITY = -5
     STAY_REWARD = 0
-    
-    # 创建一个字典，用于存放agent的RGB
-    d = {1: (255, 0, 0),  # blue
-         2: (0, 255, 0),  # green
-         3: (0, 0, 255)}  # red
-    
-    PLAYER_N = 1
-    FOOD_N = 2
-    ENEMY_N = 3
 
-    def __init__(self,player_positions) -> None:
-        self.enemy_positions  = [  (5,8),(6,8), 
-                         (5,9),(6,9),
-                         (5,10),(6,10),
-
-                         (9,2), (10,2),(11,2),(12,2),(13,2), (14,2),(15,2),(16,2),(17,2),(9,3),(9,4),
-                         
-                         (22,5), (23,5),(24,5),
-                         (22,6), (23,6),(24,6),
-                         (22,7), (23,7),(24,7), 
-                         (22,8), (23,8),(24,8),
-                         (22,9), (23,9),(24,9),
-
-                         (11,10),(12,10),(13,10),(14,10),(15,10),(16,10),
-                         (11,11),(12,11),
-                         (11,12),(12,12),
-                         (11,13),(12,13),
-                         
-                         (3,20),(4,20),(5,20),
-                         (3,21),(4,21),(5,21),(3,23),(3,24),(3,25),
-                         (3,22),(4,22),(5,22),
-
-                         (14,20), (14,21),(14,22),(14,23), (14,24),(14,25),(14,26),(14,27),
-                                          (15,22),(15,23), (15,24),(15,25),(15,26),(15,27),
-                         
-                         (21,17), (22,17),(23,17),(24,17), (25,17),(26,17),
-                         (21,18), (22,18),(23,18),(24,18), (25,18),(26,18),(26,19),
-                         (26,20),(26,21),(26,22),(26,23),(26,24)]  # 指定enemy的位置
+    def __init__(self,player_positions,enemy_positions) -> None:
+        self.enemy_positions = enemy_positions
         self.player_positions = player_positions
-        self.NUM_PLAYERS = len(self.player_positions)   # player的数量
-        self.NUM_ENEMIES = len(self.enemy_positions)   # enemy的数量
-
+        self.NUM_PLAYERS = len(player_positions)    # player的数量
+        self.NUM_ENEMIES = len(enemy_positions)     # enemy的数量
         self.OBSERVATION_SPACE_VALUES = (2+2*self.NUM_ENEMIES)*self.NUM_PLAYERS  # state的数量
+
     def reset(self):
         self.trajectory = [[] for _ in range(self.NUM_PLAYERS)]          # 在每个步骤开始之前清空轨迹列表
         
-        self.old_distances = 0
+        self.old_distances = float('inf')  # 初始化为正无穷大
 
         self.food = Cube(self.SIZE,23,23)       # 创建food
 
@@ -150,8 +108,8 @@ class envCube:  # 生成环境类
         self.enemies = []                       # 创建enemy
         for i in range(self.NUM_ENEMIES):
             x, y = self.enemy_positions[i]  
-            enemy = Cube(self.SIZE, x, y)  
-            self.enemies.append(enemy)
+            self.enemy = Cube(self.SIZE, x, y)  
+            self.enemies.append(self.enemy)
 
         state = ()                              # 记录状态
         for i in range(self.NUM_PLAYERS):
@@ -162,12 +120,11 @@ class envCube:  # 生成环境类
 
         return state
 
-    def step(self, action):
+    def step(self, player_id,action):
         equal_p_e = False
         self.episode_step += 1        
 
-        for i in range(self.NUM_PLAYERS):
-            self.players[i].action(action)
+        self.players[player_id].action(action)
 
         if ENV_MOVE == True:
             self.food.move()
@@ -175,23 +132,26 @@ class envCube:  # 生成环境类
                 enemy.move()
    
         new_observation = () 
+
         new_distances = [] #下一步的每个agent和food的距离之和
         for i in range(self.NUM_PLAYERS):
-            new_observation += (self.players[i] - self.food)
-                # 考虑和每个enemy的位置关系
-            new_distances = np.linalg.norm(new_observation, ord=1)     #计算agent和food的距离
+            new_observation += (self.players[i] - self.food)    # 更新state
             for j in range(self.NUM_ENEMIES):
                 new_observation += (self.players[i] - self.enemies[j])
             
-        # 判断player和enemy是否重叠
-        if self.old_distances>new_distances:
+            distance = np.linalg.norm(new_observation, ord=1)   # 计算代理和食物的距离
+            new_distances.append(distance)
+            
+        new_distances_sum = np.sum(new_distances)
+
+        if self.old_distances>new_distances_sum:
             reward = self.CLOSER_REWARD
-        elif self.old_distances<new_distances:
+        elif self.old_distances<new_distances_sum:
             reward = self.FARER_PENALITY
         else:
             reward = self.STAY_REWARD
         
-        self.old_distances = new_distances
+        self.old_distances = new_distances_sum
 
         for i in range(self.NUM_PLAYERS):           #定义奖励机制
             for j in range(self.NUM_ENEMIES):
@@ -210,7 +170,7 @@ class envCube:  # 生成环境类
         for i in range(self.NUM_PLAYERS):
             self.trajectory[i].append((self.players[i].get_x(), self.players[i].get_y()))
 
-        #所有玩家被吃掉/都到达/超过200步，游戏结束
+        #任意一个玩家被吃掉/都到达/超过200步，游戏结束
         for i in range(self.NUM_PLAYERS):
             for j in range(self.NUM_ENEMIES):
                 if self.players[i] == self.food or self.players[i] == self.enemies[j] or self.episode_step >= MAX_STEP:
@@ -224,7 +184,7 @@ class envCube:  # 生成环境类
 
         return new_observation, reward, done
         
-    def render_trajectory(self, flag):
+    def render_trajectory(self, flag):          #记录agent轨迹
         fig, ax = plt.subplots(figsize=(8, 8))
         ax.set_xlim(0, self.SIZE)
         ax.set_ylim(0, self.SIZE)
@@ -258,62 +218,10 @@ class envCube:  # 生成环境类
             ax.plot(x, y, color=colors[i], linewidth=5)
 
         if flag==1:
-            plt.savefig("trajectory_1.png")  # 保存图像到文件
+            plt.savefig("/mnt/c/Users/asus/Desktop/trajectory_picture/trajectory_1.png")  # 保存图像到文件
         if flag==2:
             plt.savefig("trajectory_2.png")  # 保存图像到文件
         if flag==3:
             plt.savefig("trajectory_3.png")  # 保存图像到文件
         plt.close(fig)
-
-    def get_agent_position(self, agent_id):
-        agent = self.players[agent_id]
-        return agent.get_x(), agent.get_y()
-##############################################################################
-
-
-    def render(self):                   #显示图片
-        env = np.zeros((self.SIZE, self.SIZE, 3), dtype=np.uint8)
-        env[:, :] = (255, 255, 255)  # 设置背景颜色为白色
-
-        env[self.food.get_x()][self.food.get_y()] = self.d[self.FOOD_N]
-
-        for i in range(self.NUM_PLAYERS):        
-            env[self.players[i].get_x()][self.players[i].get_y()] = self.d[self.PLAYER_N]
-
-        for i in range(self.NUM_ENEMIES):
-            env[self.enemies[i].get_x()][self.enemies[i].get_y()] = self.d[self.ENEMY_N]
-
-        img = Image.fromarray(env, 'RGB')
-
-        img = img.resize((800, 800))
-        cv2.imshow('Predator', np.array(img))
-        cv2.waitKey(1)
-
-# 用于qlearning
-    def get_qtable(self, qtable_name=None): #搭建q table表格
-        if qtable_name is None:
-            q_table = {}
-
-            def initialize_q_table(dimensions, ACTION_SPACE_VALUES):##定义q_table初始化函数
-                q_table = {}
-                recursive_initialize_q_table(
-                    dimensions, ACTION_SPACE_VALUES, [], q_table)
-                return q_table
-
-            def recursive_initialize_q_table(dimensions, ACTION_SPACE_VALUES, indices, q_table):
-                if len(indices) == len(dimensions):
-                    q_table[tuple(indices)] = [np.random.uniform(-5, 0)
-                                               for _ in range(ACTION_SPACE_VALUES)]
-                else:
-                    for i in range(-dimensions[len(indices)] + 1, dimensions[len(indices)]):
-                        indices.append(i)
-                        recursive_initialize_q_table(
-                            dimensions, self.ACTION_SPACE_VALUES, indices, q_table)
-                        indices.pop()
-
-            dimensions = self.NUM_PLAYERS*(self.NUM_ENEMIES*2*[self.SIZE]+[self.SIZE]+[self.SIZE])
-            q_table = initialize_q_table(dimensions, self.ACTION_SPACE_VALUES)     
-        else:
-            with open(qtable_name, 'rb') as f:
-                q_table = pickle.load(f)
-        return q_table
+        
